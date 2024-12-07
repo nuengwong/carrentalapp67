ee4b1fcfa6c2e416bb7b039e2fc01f0cd2f4acd0
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2024-12-07 20:27:52 +0700

    Chapter10

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,11 +3,15 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './banner.module.css';
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 export default function Banner() {
   const covers = ['/img/cover.jpg', '/img/cover2.jpg', '/img/cover3.jpg'];
   const [index, setIndex] = useState(0);
   const router = useRouter();
+
+  const { data: session } = useSession();
+  console.log(session?.user.token);
 
   return (
     <div
@@ -26,6 +30,12 @@ export default function Banner() {
         <h1 className="text-4xl font-medium">Your Travel Partner</h1>
         <h3 className="text-xl font-serif">Explore Your World with Us</h3>
       </div>
+      {session ? (
+        <div className="z-30 absolute top-5 right-10 font-semibold text-cyan-800 text-xl">
+          Hello {session.user?.name}
+        </div>
+      ) : null}
+
       <button
         className="bg-white text-cyan-600 border border-cyan-600 
       font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
